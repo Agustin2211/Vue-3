7f6f34b5282075bffeba452467f1860c7e15742e
Agustin2211 <agustinkowalski@gmail.com>
2021-09-07 20:00:39 -0300

    Llamando a la API

--- rickandmorty--api-rest/src/store/index.js
+++ rickandmorty--api-rest/src/store/index.js
@@ -2,10 +2,28 @@ import { createStore } from 'vuex'
 
 export default createStore({
   state: {
+    chracters: [], //Guarda todo lo que tengo
+    charactersFilter: [] //Se va filtrando lo que tengo a medida que escribo
   },
-  mutations: {
+  mutations: { //Modifican los State
+    setCharacters(state, payload){
+      state.characters = payload
+    },
+    setCharactersFilter(state, payload){
+      state.charactersFilter = payload
+    }
   },
   actions: {
+    async getCharacters(commit){
+      try{
+        //Obtener datos de la API
+        const response = await fetch ("https://rickandmortyapi.com/api/character")
+        const data = await response.json()
+        console.log
+      }catch(error){
+        console.error(error)
+      }
+    }
   },
   modules: {
   }
